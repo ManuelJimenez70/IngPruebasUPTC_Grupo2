e51f53b0f4f29ae0f5a2a7d354cf2120be91c631
Manuel Jimenez <manuel.jimenez01@uptc.edu.co>
2023-10-19 15:43:38 -0500

    creacion primeros casos de prueba

--- cypress/e2e/Taller/sesion18.cy.js
+++ cypress/e2e/Taller/sesion18.cy.js
@@ -1,16 +1,37 @@
-describe('Registro de sesion', () => {
+
+describe('Inicio de sesion', () => {
     beforeEach(() => {
-      cy.visit('https://petstore.octoperf.com/actions/Catalog.action')
+        cy.visit('https://petstore.octoperf.com/actions/Catalog.action')
     })
-  
+
     afterEach(() => {
-      cy.window().then((win) => {
-        win.close();
-      });
+        cy.window().then((win) => {
+            win.close();
+        });
     });
 
-    it('Registro de sesion correctamente', () => {
-       cy.click("")
+    it('Inicio de sesion correctamente', () => {
+        cy.get('a').contains('Sign In').click("center")
+        cy.get("[name = 'username']").type("11111");
+        cy.get("[name = 'password']").clear().type("prueba123*");
+        cy.get("[name = 'signon']").click("center")
+        cy.get('a').contains('Sign Out').should("be.visible")
     })
 
-  })
+    it('Inicio de sesion - contraseña incorrecta', () => {
+        cy.get('a').contains('Sign In').click("center")
+        cy.get("[name = 'username']").type("11111");
+        cy.get("[name = 'password']").clear().type("prueba123");
+        cy.get("[name = 'signon']").click("center")
+        cy.get('li').contains('Signon failed.').should("be.visible")
+    })
+
+    it('Inicio de sesion - usuario inexistente', () => {
+        cy.get('a').contains('Sign In').click("center")
+        cy.get("[name = 'username']").type("11111123321312");
+        cy.get("[name = 'password']").clear().type("prueba123");
+        cy.get("[name = 'signon']").click("center")
+        cy.get('li').contains('Signon failed.').should("be.visible")
+    })
+
+})
