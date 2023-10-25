11d4157f7fd29093039c5cd0a2d9d09491ee63c6
Manuel Jiménez <manuel.jimenez01@uptc.edu.co>
2023-10-24 22:45:33 -0500

    creacion de casos de prueba - compra producto

--- cypress/e2e/Taller/sesion18.cy.js
+++ cypress/e2e/Taller/sesion18.cy.js
@@ -1,3 +1,74 @@
+describe('Registro', () => {
+
+    beforeEach(() => {
+        cy.visit('https://petstore.octoperf.com/actions/Catalog.action')
+    })
+
+    it('Registro correcto', () => {
+        cy.get('a').contains('Sign In').click("center");
+        cy.get('a').contains('Register Now!').click("center");
+        cy.get("[name = 'username']").clear().type("544623994194");
+        cy.get("[name = 'password']").clear().type(generateRandomNumberString());
+        cy.get("[name = 'repeatedPassword']").clear().type("12345678");
+        cy.get("[name='account.firstName']").type("Nombre prueba");
+        cy.get("[name = 'account.lastName']").clear().type("Apellido Prueba");
+        cy.get("[name='account.email']").type("prueba@prueba.com");
+        cy.get("[name = 'account.phone']").clear().type("1234567890");
+        cy.get("[name = 'account.address1']").clear().type("calle 1 # 2-3");
+        cy.get("[name = 'account.address2']").clear().type("calle 4 # 5-6");
+        cy.get("[name = 'account.city']").clear().type("Tunja");
+        cy.get("[name = 'account.state']").clear().type("Boyaca");
+        cy.get("[name = 'account.zip']").clear().type("a");
+        cy.get("[name = 'account.country']").clear().type("Col");
+        cy.get("[name = 'newAccount']").click("center");
+        cy.get('a').contains('Sign In').should("be.visible");
+    })
+    const generateRandomNumberString = () => {
+        const randomNumber = Math.floor(Math.random() * 1000000000000);
+        return randomNumber.toString();
+    };
+
+    it('Registro con usuario existente', () => {
+        cy.get('a').contains('Sign In').click("center")
+        cy.get('a').contains('Register Now!').click("center")
+        cy.get("[name = 'username']").type("11111");
+        cy.get("[name = 'password']").clear().type("12345678");
+        cy.get("[name = 'repeatedPassword']").clear().type("12345678");
+        cy.get("[name='account.firstName']").type("Nombre prueba");
+        cy.get("[name = 'account.lastName']").clear().type("Apellido Prueba");
+        cy.get("[name='account.email']").type("prueba@prueba.com");
+        cy.get("[name = 'account.phone']").clear().type("1234567890");
+        cy.get("[name = 'account.address1']").clear().type("calle 1 # 2-3");
+        cy.get("[name = 'account.address2']").clear().type("calle 4 # 5-6");
+        cy.get("[name = 'account.city']").clear().type("Tunja");
+        cy.get("[name = 'account.state']").clear().type("Boyaca");
+        cy.get("[name = 'account.zip']").clear().type("a");
+        cy.get("[name = 'account.country']").clear().type("Col");
+        cy.get("[name = 'newAccount']").click("center");
+        cy.get('h1').contains("Error").should("be.visible");
+    })
+
+    it('Registro - confirmación de contraseñas negativo', () => {
+        cy.get('a').contains('Sign In').click("center")
+        cy.get('a').contains('Register Now!').click("center")
+        cy.get("[name = 'username']").type(generateRandomNumberString());
+        cy.get("[name = 'password']").clear().type("12345678");
+        cy.get("[name = 'repeatedPassword']").clear().type("12345679");
+        cy.get("[name='account.firstName']").type("Nombre prueba");
+        cy.get("[name = 'account.lastName']").clear().type("Apellido Prueba");
+        cy.get("[name='account.email']").type("prueba@prueba.com");
+        cy.get("[name = 'account.phone']").clear().type("1234567890");
+        cy.get("[name = 'account.address1']").clear().type("calle 1 # 2-3");
+        cy.get("[name = 'account.address2']").clear().type("calle 4 # 5-6");
+        cy.get("[name = 'account.city']").clear().type("Tunja");
+        cy.get("[name = 'account.state']").clear().type("Boyaca");
+        cy.get("[name = 'account.zip']").clear().type("a");
+        cy.get("[name = 'account.country']").clear().type("Col");
+        cy.get("[name = 'newAccount']").click("center");
+        cy.get('a').contains('Sign In').should("be.visible");
+    })
+}
+)
 
 describe('Inicio de sesion', () => {
     beforeEach(() => {
@@ -14,38 +85,38 @@ describe('Inicio de sesion', () => {
         cy.get('a').contains('Sign In').click("center")
         cy.get("[name = 'username']").type("11111");
         cy.get("[name = 'password']").clear().type("prueba123*");
-        cy.get("[name = 'signon']").click("center")
-        cy.get('a').contains('Sign Out').should("be.visible")
+        cy.get("[name = 'signon']").click("center");
+        cy.get('a').contains('Sign Out').should("be.visible");
     })
 
-    it('Inicio de sesion - contraseña incorrecta', () => {
-        cy.get('a').contains('Sign In').click("center")
-        cy.get("[name = 'username']").type("11111");
-        cy.get("[name = 'password']").clear().type("prueba123");
-        cy.get("[name = 'signon']").click("center")
-        cy.get('li').contains('Signon failed.').should("be.visible")
-    })
-
-    it('Inicio de sesion - usuario inexistente', () => {
-        cy.get('a').contains('Sign In').click("center")
-        cy.get("[name = 'username']").type("11111123321312");
-        cy.get("[name = 'password']").clear().type("prueba123");
-        cy.get("[name = 'signon']").click("center")
-        cy.get('li').contains('Signon failed.').should("be.visible")
-    })
-
+    it('Inicio de sesion negativos', () => {
+        cy.fixture('user.json').then((userFixture) => {
+            let couter = 1;
+            userFixture.forEach(element => {
+                cy.get('a').contains('Sign In').click("center");
+                cy.get("[name='username']").type(element.username);
+                cy.get("[name='password']").clear().type(element.password);
+                cy.get("[name='signon']").click("center");
+                cy.screenshot("inicio de sesion" + couter, { capture: 'fullPage' , blackout: ["[name='password']"] });
+                cy.get('li').contains('Signon failed.').should("be.visible");
+                couter++;
+            });
+        });
+    });
 })
 
-
-describe('Cambio de contraseña', () => {
+describe('Compra de Producto', () => {
     beforeEach(() => {
-        cy.visit('https://petstore.octoperf.com/actions/Catalog.action')
+        cy.visit('https://petstore.octoperf.com/actions/Catalog.action');
+        // Agregar Producto al carrito
         cy.get('a').contains('Sign In').click("center")
         cy.get("[name = 'username']").type("11111");
         cy.get("[name = 'password']").clear().type("prueba123*");
-        cy.get("[name = 'signon']").click("center")
-        cy.get('a').contains('My Account').click("center")
-
+        cy.get("[name = 'signon']").click("center");
+        cy.get('a[href*="/actions/Catalog.action?viewCategory=&categoryId=FISH"]').first().click()  ;  
+        cy.get('a').contains('FI-SW-01').click("center");
+        cy.get('a').contains('Add to Cart').click("center");
+        cy.get("[id = 'LogoContent']").click("center");
     })
 
     afterEach(() => {
@@ -54,31 +125,20 @@ describe('Cambio de contraseña', () => {
         });
     });
 
+    it('Compra Exitosa', () => {
+        cy.get("img[name = 'img_cart']").click("center");
+        cy.get('a').contains('Proceed to Checkout').click("center");
+        cy.get("[name = 'newOrder']").click("center");
+        cy.get('a').contains('Confirm').click("center");
+        cy.get('li').contains('Thank you, your order has been submitted.').should("be.visible");
+    })
 
-
-})
-
-
-describe('Registro', () => {
-
-    it('Registro correcto', () => {
-        cy.visit('https://petstore.octoperf.com/actions/Catalog.action')
-        cy.get('a').contains('Sign In').click("center")
-        cy.get('a').contains('Register Now!').click("center")
-
-        cy.get("[name = 'username']").type("00005");
-        cy.get("[name = 'password']").clear().type("12345678");
-        cy.get("[name = 'repeatedPassword']").clear().type("12345678");
-        cy.get("[name='account.firstName']").type("Nombre prueba");
-        cy.get("[name = 'account.lastName']").clear().type("Apellido Prueba");
-        cy.get("[name='account.email']").type("prueba@prueba.com");
-        cy.get("[name = 'account.phone']").clear().type("1234567890");
-
-        cy.get("[name = 'account.address1']").clear().type("calle 1 # 2-3");
-        cy.get("[name = 'account.address2']").clear().type("calle 4 # 5-6");
-
-        cy.get("[name = 'account.city']").clear().type("Tunja");
-
-
+    it('Compra con existencias maximas', () => {
+        cy.get("img[name = 'img_cart']").click("center");
+        cy.get("input[name = 'EST-1']").clear().type("1000000000000000000000000{enter}");
+        cy.get('a').contains('Proceed to Checkout').click("center");
+        cy.get("[name = 'newOrder']").click("center");
+        cy.get('a').contains('Confirm').click("center");
+        cy.get('li').contains('Thank you, your order has been submitted.').should("be.visible");
     })
 })
