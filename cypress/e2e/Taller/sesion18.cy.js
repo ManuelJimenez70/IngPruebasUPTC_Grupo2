d0004821408c7ee6c6c499f1c162b85778184aa5
Manuel Jiménez <manuel.jimenez01@uptc.edu.co>
2023-10-24 19:55:31 -0500

    correccion test Registro

--- cypress/e2e/Taller/sesion18.cy.js
+++ cypress/e2e/Taller/sesion18.cy.js
@@ -62,6 +62,7 @@ describe('Cambio de contraseña', () => {
 describe('Registro', () => {
 
     it('Registro correcto', () => {
+        cy.visit('https://petstore.octoperf.com/actions/Catalog.action')
         cy.get('a').contains('Sign In').click("center")
         cy.get('a').contains('Register Now!').click("center")
 
